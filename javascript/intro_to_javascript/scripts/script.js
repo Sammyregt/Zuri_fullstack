5478ab467380b55a7587dfb8b7a073db17e82ad3
Sammyregt <adeoyesamuel100@gmail.com>
2022-06-24 08:19:36 -0700

    Intro to java

--- javascript/intro_to_javascript/scripts/script.js
+++ javascript/intro_to_javascript/scripts/script.js
@@ -1,7 +1,7 @@
 let name = 'Samuel';
-let height = '6 inches'
-let country = 'Nigeria'
+let height = '6 inches';
+let country = 'Nigeria';
 
-console.log(name)
-console.log(height)
-console.log(country)
+console.log(name);
+console.log(height);
+console.log(country);
